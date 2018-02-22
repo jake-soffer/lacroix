96af94aa58e4922dc4da01f4170a97dbdb911079
jbs <soffer94@gmail.com>
2018-02-21 20:25:20 -0800

    Complete next button functionality.

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,32 @@
 $(document).ready(function()  {
   $("#namenext").click(function(event) {
     event.preventDefault();
-    $("#question-name").fadeOut();
-    $("#question-age").fadeIn();
+    $("#question-name").hide();
+    $("#question-age").show();
   });
 
   $("#agenext").click(function(event) {
     event.preventDefault();
-    $("#question-age").fadeOut();
-    $("#question-favcolor").fadeIn();
+    $("#question-age").hide();
+    $("#question-favcolor").show();
   });
 
   $("#favcolornext").click(function(event) {
     event.preventDefault();
-    $("#question-favcolor").fadeOut();
-    $("#question-wives").fadeIn();
+    $("#question-favcolor").hide();
+    $("#question-birthday").show();
+  });
+
+  $("#birthdaynext").click(function(event) {
+    event.preventDefault();
+    $("#question-birthday").hide();
+    $("#question-wives").show();
   });
 
   $("#wivesnext").click(function() {
     event.preventDefault();
-    $("#question-wives").fadeOut();
-    $("#question-sparkling").fadeIn();
+    $("#question-wives").hide();
+    $("#question-sparkling").show();
   });
 
   $("form#questions").submit(function(event)  {
@@ -29,11 +35,28 @@ $(document).ready(function()  {
     var name = $("#name").val();
     var age = parseInt($("#age").val());
     var favColor = $("input:radio[name=favcolor]:checked").val();
+    var birthday = $("input:radio[name=birthday]:checked").val();
     var wives = $("input:radio[name=wives]:checked").val();
     var sparkling = $("input:radio[name=sparkling]:checked").val();
 
     if (favColor === "blue") {
-      $("#pure").show();
+      $("#pure").fadeIn();
+    }
+
+    if (favColor === "yellow") {
+      $("#lemon").fadeIn();
+    }
+
+    if (favColor === "purple") {
+      $("#berry").fadeIn();
+    }
+
+    if (favColor === "orange") {
+      $("#pamplemousse").fadeIn();
+    }
+
+    if (favColor === "pink") {
+      $("#cranraspberry").fadeIn();
     }
   });
 });
